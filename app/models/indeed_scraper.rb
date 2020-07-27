d9ba5e68e0c61191aa7ecced2b8c95c0ffb671aa
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-27 15:47:28 -0400

    Building Webscraper Template

---
+++ app/models/indeed_scraper.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+require 'nokogiri'
+
+class IndeedScraper
+
+
+end
+
+ # Testing the following URL - in later version, we should make this dynamic to pass in user params for job title and location
+ URL = "https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY&start=10"
+
+# requesting resource
+# html = URI.open(URL)
+page = Nokogiri::HTML(URI.open(URL))
+jobs = page.css(".jobsearch-SerpJobCard").text#.css('title')
+pp jobs
+
+# def extract_job_title_from_result(noko_page)
+# end
