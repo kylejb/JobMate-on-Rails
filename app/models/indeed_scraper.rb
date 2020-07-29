37758e328ef64e0c285df8ad007878c765655789
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-29 11:15:34 -0400

    Significant Scraping/Crawling Updates

--- app/models/indeed_scraper.rb
+++ app/models/indeed_scraper.rb
@@ -14,42 +14,57 @@ class IndeedScraper < Kimurai::Base
         @@jobs
     end
 
+    def parse(response, url:, data: {})
+        # scrape first page
+       scrape_page
+       # next page link starts with 10 so the counter will be initially set to 1
+       num = 1
+       #visit next page and scrape it
+       # TODO - make this run until end
+       2.times do
+           browser.visit("https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY&sort=date&fromage=14&start=#{num}0")
+           scrape_page
+           num += 1
+       end
+       @@jobs
+    end
+
     def scrape_page
         doc = browser.current_response
         returned_jobs = doc.css('td#resultsCol')
+        count = 0
         returned_jobs.css('div.jobsearch-SerpJobCard').each do |char_element|
-          # scraping individual listings 
-          title = char_element.css('h2 a')[0].attributes["title"].value.gsub(/\n/, "")
-          link = "https://indeed.com" + char_element.css('h2 a')[0].attributes["href"].value.gsub(/\n/, "")
-          description = char_element.css('div.summary').text.gsub(/\n/, "")
-          company = description = char_element.css('span.company').text.gsub(/\n/, "")
-          location = char_element.css('div.location').text.gsub(/\n/, "")
-          salary = char_element.css('div.salarySnippet').css('span.salaryText').text.gsub(/\n/, "")
-          requirements = char_element.css('div.jobCardReqContainer').text.gsub(/\n/, "")
+            #! boolean check for existence in our db before stepping into char_ele
+            # scraping individual listings 
+            title = char_element.css('h2 a')[0].attributes["title"].value.gsub(/\n/, "")
+            company = description = char_element.css('span.company').text.gsub(/\n/, "")
+            link = "https://indeed.com" + char_element.css('h2 a')[0].attributes["href"].value.gsub(/\n/, "")
+        #   description = char_element.css('div.summary').text.gsub(/\n/, "")
+        #   location = char_element.css('div.location').text.gsub(/\n/, "")
+        #   salary = char_element.css('div.salarySnippet').css('span.salaryText').text.gsub(/\n/, "")
+        #   requirements = char_element.css('div.jobCardReqContainer').text.gsub(/\n/, "")
+
+          # click on link and extract description, salary, and location
+          browser.visit(link)
+          job_page = browser.current_response
+          description = job_page.xpath('/html/body/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[5]/div[2]').text
+         
           # creating a job object
-          job = {title: title, link: link, description: description, company: company, location: location, salary: salary, requirements: requirements}
+          job = {title: title, link: link, description: description, company: company} #, location: location, salary: salary, languages: languages, experience: experience, requirements: requirements}
         
           # adding the object if it is unique
           #! test with byebug to see if include is behaving correctly//'find_by'
           @@jobs << job if !@@jobs.include?(job)
+          p count += 1
         end
+        puts "There are #{count} jobs in class variable @@jobs."
     end
 
-    def parse(response, url:, data: {})
-         # scrape first page
-        scrape_page
-        # next page link starts with 10 so the counter will be initially set to 1
-        num = 1
-        #visit next page and scrape it
-        # TODO - make this run until end
-        10.times do
-            browser.visit("https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY&sort=date&fromage=14&start=#{num}0")
-            scrape_page
-            num += 1
-        end
-        @@jobs
+    def add_to_db
+        # .create()
     end
 end
+
 IndeedScraper.crawl!
 
 
