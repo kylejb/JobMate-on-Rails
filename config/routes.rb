0861f55a76c97f464be9be0c88369a306d0c3033
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-29 18:21:45 -0400

    initial styling for login page and some refactoring of code

--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,31 @@ Rails.application.routes.draw do
   resources :categories, only: [:show, :index]
   #resources :favorite_companies
   resources :companies, only: [:show, :index]
-  resources :saved_postings, only: :index
+  resources :saved_postings, only: [:index]
   resources :postings, only: [:index, :destroy]
   resources :users, except: [:index, :new, :create]
 
   root 'home#dashboard'
 
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create', as: 'new_session'
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'delete_session'
 
-  get '/signup', to: "users#new", as: "signup"
-  post '/users', to: 'users#create', as: 'users'
+  get '/signup', to: 'users#new', as: 'new_user'
+  post '/signup', to: 'users#create'
 
   #Posting.all has all postings. 
   #When you click 'search', you narrow it by titles and locations that match
-  get '/postings/search', to: "postings#search", as: "search_postings"
+  get '/postings/search', to: 'postings#search', as: 'search_postings'
 
-  get '/postings/search_results', to: "postings#search_results", as: "display_postings"
-
-
+  get '/postings/search_results', to: 'postings#search_results', as: 'display_postings'
 
 # Form-tag to allow user to search for jobs
-  get '/search', to: "searches#new", as: "new_search"
+  get '/search/new', to: 'searches#new', as: 'new_search'
 
 # Runs the scraper based on user's search parameters
-  post '/search/result', to: "searches#create", as: "run_search"
+  get '/search/result', to: 'searches#show', as: 'show_search'
+  post '/search/result', to: 'searches#create'
 
-  get '/search/result', to: "searches#show", as: "show_search"
+  
 end
