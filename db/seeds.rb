c18cba04ff051f5b29b2e932b2249d0be842a4db
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-31 10:35:21 -0400

    Updated Seed file for sample associatons for recommendation feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@ User.destroy_all
 # Random User Creation with Random Category to demo our recommendation feature
 (1..60).each do |i|
     User.create!(username: Faker::Name.unique.name, password: "testuser#{i}", password_confirmation: "testuser#{i}", category: Category.all.sample)
-    p "user loop"
+end
+
+(1..200).each do |i|
+    SavedPosting.create!(user: User.all.sample, posting: Posting.all.sample)
 end
